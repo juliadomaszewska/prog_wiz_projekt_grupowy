a17a1f3e7450c7eead2b1815f451a5139a6bac58
Julia <julia.domaszewska@student.put.poznan.pl>
2026-04-30 10:50:33 +0200

    Utworzenie menu do wyboru gier

--- prog_wiz_lab8/Views/MainWindow.axaml.cs
+++ prog_wiz_lab8/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace prog_wiz_lab8.Views;
 
@@ -7,5 +8,21 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void BtnGame1_Click(object? sender, RoutedEventArgs e)
+    {
+        // Tutaj otwierać się będzie okno Gry 1 (robionej przez Ciebie lub koleżankę)
+        // np. new Game1Window().Show();
+    }
+
+    private void BtnGame2_Click(object? sender, RoutedEventArgs e)
+    {
+        // Tutaj otwierać się będzie okno Gry 2
+    }
+
+    private void BtnGame3_Click(object? sender, RoutedEventArgs e)
+    {
+        // Tutaj otwierać się będzie okno Gry 3
     }
 }
